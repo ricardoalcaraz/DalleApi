40a1b55127186a35134d25ab94a13fdcb56c1ae7
ralcaraz <ralcarazerhs@gmail.com>
2022-07-01 17:42:19 -0700

    Added API project

---
+++ DalleProxy/ImageWatcherService.cs
@@ -0,0 +1,74 @@
+namespace DalleProxy;
+
+public class ImageWatcherService : IDisposable
+{
+    private readonly FileSystemWatcher _watcher;
+
+    public ImageWatcherService()
+    {
+        _watcher = new FileSystemWatcher(@"C:\path\to\folder");
+
+        _watcher.NotifyFilter = NotifyFilters.Attributes
+                               | NotifyFilters.CreationTime
+                               | NotifyFilters.DirectoryName
+                               | NotifyFilters.FileName
+                               | NotifyFilters.LastAccess
+                               | NotifyFilters.LastWrite
+                               | NotifyFilters.Security
+                               | NotifyFilters.Size;
+
+        _watcher.Created += OnCreated;
+        _watcher.Deleted += OnDeleted;
+        _watcher.Error += OnError;
+
+        _watcher.Filter = "*.txt";
+        _watcher.IncludeSubdirectories = true;
+        _watcher.EnableRaisingEvents = true;
+    }
+    
+    
+    private static void OnChanged(object sender, FileSystemEventArgs e)
+    {
+        if (e.ChangeType != WatcherChangeTypes.Changed)
+        {
+            return;
+        }
+        Console.WriteLine($"Changed: {e.FullPath}");
+    }
+
+    private static void OnCreated(object sender, FileSystemEventArgs e)
+    {
+        var value = $"Created: {e.FullPath}";
+        Console.WriteLine(value);
+    }
+
+    private static void OnDeleted(object sender, FileSystemEventArgs e) =>
+        Console.WriteLine($"Deleted: {e.FullPath}");
+
+    private static void OnRenamed(object sender, RenamedEventArgs e)
+    {
+        Console.WriteLine($"Renamed:");
+        Console.WriteLine($"    Old: {e.OldFullPath}");
+        Console.WriteLine($"    New: {e.FullPath}");
+    }
+
+    private static void OnError(object sender, ErrorEventArgs e) =>
+        PrintException(e.GetException());
+
+    private static void PrintException(Exception? ex)
+    {
+        if (ex != null)
+        {
+            Console.WriteLine($"Message: {ex.Message}");
+            Console.WriteLine("Stacktrace:");
+            Console.WriteLine(ex.StackTrace);
+            Console.WriteLine();
+            PrintException(ex.InnerException);
+        }
+    }
+    
+    public void Dispose()
+    {
+        _watcher.Dispose();
+    }
+}
