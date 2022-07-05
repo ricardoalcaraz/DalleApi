40380d2506fd2d15d22dd02af34b20d32db14684
ralcaraz <ralcarazerhs@gmail.com>
2022-07-04 17:43:51 -0700

    updated packages

--- DalleApi/Program.cs
+++ DalleApi/Program.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHostedService<ImportantService>();
 
 var app = builder.Build();
 
@@ -23,3 +26,56 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+public class ImportantService : BackgroundService
+{
+    private readonly ILogger _logger;
+
+    // use of dependency injection ILogger and IHostApplicationLifetime
+    public ImportantService(ILogger<ImportantService> logger,
+        IHostApplicationLifetime applicationLifetime)
+    {
+        _logger = logger;
+
+        // register to lifetime callbacks
+        applicationLifetime.ApplicationStarted.Register(OnStarted);
+        applicationLifetime.ApplicationStopped.Register(OnStopped);
+        applicationLifetime.ApplicationStopping.Register(OnStopping);
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        var proc = new Process {
+            StartInfo = new ProcessStartInfo {
+                FileName = "/bin/bash",
+                Arguments = "",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true
+            }
+        };
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            _logger.LogInformation("Doing very important stuff...");
+
+            await Task.Delay(TimeSpan.FromSeconds(1), stoppingToken);
+        }
+
+        _logger.LogInformation("Oops, cancellation was requested!");
+    }
+
+    private void OnStarted()
+    {
+        _logger.LogInformation($"Executing: {nameof(OnStarted)}, I should get ready to work!");
+    }
+
+    private void OnStopping()
+    {
+        _logger.LogInformation($"Executing: {nameof(OnStopping)}, I should block stopping and clean up!");
+    }
+
+    private void OnStopped()
+    {
+        _logger.LogInformation($"Executing: {nameof(OnStopped)}, I should stop!");
+    }
+}
